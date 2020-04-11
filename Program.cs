3e409b811964a9e136d7a879be35f403bba782d9
kunal <31889898+chalotrakunal@users.noreply.github.com>
2020-04-11 14:31:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students student = new Students();
+            Console.WriteLine("How many no of  student records you want : ");          //specify  the count of student for which we want to store the record
+            var record = Console.ReadLine();
+            int noOfStudent = Convert.ToInt32(record);
+            for (int k = 0; k < noOfStudent; k++)
+            {
+                Console.WriteLine("Enter the name of student :");           // enter name
+                var studentName = Console.ReadLine();
+                Console.WriteLine("Enter the Id of student :");            // enter Id
+                var id = Console.ReadLine();
+                int studentId = Convert.ToInt32(id);
+                Console.WriteLine("Enter the marks scored by student :");  // enter the marks
+                var marks = Console.ReadLine();
+                int studentMarks = Convert.ToInt32(marks);
+                student.StoreStudentRecords(studentName, studentId, studentMarks);    // call to method which will store the stident records
+            }
+          
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Rank of the student after sorting  based on marks are as follow :");
+            Console.WriteLine("----------------------------------------");
+            student.SortStudentRecords();
+            student.DisplayStudentRecords();
+            
+        }
+
+        
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    struct StudentRecord
+    {
+        public string _studentName;
+        public int _studentId;
+        public int _studentMarks;
+        public StudentRecord(string studentName, int studentId, int studentMarks)
+        {
+            _studentName  = studentName;
+            _studentId    = studentId;
+            _studentMarks = studentMarks;
+        }
+    }
+    public class Students
+    {
+        List<StudentRecord> records = new List<StudentRecord>();
+        List<StudentRecord> studrecords;
+        public void StoreStudentRecords(string studentName, int studentId, int studentMarks)     // storing each record dynamically to list of Student record
+        {
+            var record = new StudentRecord();
+            record._studentName  = studentName;
+            record._studentId    = studentId;
+            record._studentMarks = studentMarks;
+            records.Add(record);
+        }
+        public void SortStudentRecords()                                  //sorting the records
+        {
+            var linq = (from record in records orderby record._studentMarks descending select record).ToList();
+            studrecords = linq.ToList();
+        }
+        public void DisplayStudentRecords()                                 // display student records with rank
+        {
+            
+            int studentRank = 0;
+            foreach (var record in studrecords)
+            {
+                ++studentRank;
+                Console.WriteLine("Name of the student is :" + record._studentName);
+                Console.WriteLine("Id of student is : " + record._studentId);
+                Console.WriteLine("Marks of student is :" + record._studentMarks);
+                Console.WriteLine("Rank of the student is :" + studentRank);
+                Console.WriteLine("----------------------------------------");
+            }
+            Console.WriteLine("\n");
+        }
+
+    }
+}
+
+    
+
